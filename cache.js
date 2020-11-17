eb3eac26a403dab3f3c124a4e7dd34e6d0558ff0
821826173@qq.com <520201314wg>
2020-11-17 15:16:58 +0800

    ~

--- cache.js
+++ cache.js
@@ -6,9 +6,5 @@ export const setObCache = (key, value) => {
 }
 
 export const getObCache = (key) => {
-<<<<<<< HEAD
-    localStorage.getItem(key, JSON.parse(value))
-=======
     localStorage.getItem(key)
->>>>>>> a37216b409b6964a3d082e3bb2c0663f18c974a5
 }
