7e96b4467a8b5aff5ab8b3e1df8baf7f5a644b9b
821826173@qq.com <520201314wg>
2020-11-17 14:06:30 +0800

    ~

--- format.js
+++ format.js
@@ -20,4 +20,13 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+/**
+ * 获取num位的数字字符串
+ * @param {*} num 
+ */
+export const getRandomNumber = (num) => {
+  if(!num) num = 10
+  return  Math.floor((Math.random()+Math.floor(Math.random()*9+1))*Math.pow(10,num-1)) + '';
+}
+
  
