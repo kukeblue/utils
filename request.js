f8e7bfb7baabbbf89e33df76993a4d83f8f92d86
821826173@qq.com <520201314wg>
2020-12-05 15:30:09 +0800

    ~

--- request.js
+++ request.js
@@ -48,12 +48,8 @@ export  const request = ({url, data, cache, method}) => {
             //   console.log(error.response.status);
             //   console.log(error.response.headers);
             } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
               console.log(error.request);
             } else {
-              // Something happened in setting up the request that triggered an Error
               console.log('Error', error.message);
             }
             console.log(error.config);
